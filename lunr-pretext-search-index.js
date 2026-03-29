28dbdb37ab892677f2b70045f4e16bfdc527d68e
Mark Fitch <58183892+mafitch@users.noreply.github.com>
2026-03-29 09:20:55 -0800

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,266 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "section-schedule-saturday",
+  "level": "1",
+  "url": "section-schedule-saturday.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Contributed Talks Saturday, 18th April",
+  "body": " Contributed Talks Saturday, 18th April  Morning Contributed Talks        Contributed Talks 1   Applied Mathematics      Room 1  Room 2    10:15-10:30      10:35-10:50      10:55-11:10      11:15-11:30      11:35-11:50      11:55-12:10         Afternoon Contributed Talks        Statistics   Contributed Talks 2      BMH 235  BMH 239    3:15-3:30      3:35-3:50      3:50-4:05      4:10-4:25      4:30-4:45      4:50-5:05      5:10-5:25      "
+},
+{
+  "id": "section-abstracts-contributed",
+  "level": "1",
+  "url": "section-abstracts-contributed.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Abstracts: Contributed Talks",
+  "body": " Abstracts: Contributed Talks   Contributed Talk Abstracts   Your title here   First Last  University Name   Abstract    Upper and Lower Bounds on Mosaic and Tile numbers of Knots   Roland Allen  Gonzaga University  Maria Taggi Padro  Gonzaga University, Undergraduate   Mosaic Knot theory is a field where mathematical knots are placed on grid diagrams called mosaics. This was established by Dr. Lomonaco and Dr. Kauffman in Quantum Knots and Mosaics . In this work, we place improved lower and upper bounds on the size of a mosaic given only its crossing number or using crossing, braid index, and braid length. This is carried out by constructing an arbitrary diagram of a braid on a mosaic.    Your title here   First Last  University Name   Abstract    The Weighted Spanning Tree Enumerator of Multi-Partite Graphs   Zander Rasmussen  Willamette University  Peter Otto  Willamette University   The weighted spanning tree enumerator (WSTE) is the sum of weights of all spanning trees of a graph. A recent result expressed the WSTE of multi-partite graphs in terms of the WSTE of its partition graph, which greatly simplifies the computation of the WSTE of the original graph. The proof to this result is algebraic, using the Weighted Matrix Tree Theorem. In this talk, we will present the new result and the work we have done in deriving a graph theoretic justification for the result.    Your title here   First Last  University Name   Abstract    Geodesics and hyperbolicity for half-plane models of taxicab hyperbolic space   Alina Potapyev  Seattle University  Dylan Helliwell  Seattle University, Faculty  Luka Lawrence  Seattle University, Undergraduate Student  Kevin Lin  Seattle University, Undergraduate Student   We develop new geometric spaces by blending the half-plane model for the hyperbolic plane with taxicab geometry. Starting with a taxicab norm on each tangent space, the lengths of curves are analyzed, allowing us to determine geodesics, and from these, a distance function. Then we show that these spaces are hyperbolic in the sense of Gromov.    Your title here   First Last  University Name   Abstract    Applying Topological Data Analysis to Baseball Analytics: What Persistent Homology Says About the Need for Diverse Offensive Skill Sets   Levi Westeren  George Fox University  Thomas Shifley  George Fox University   Traditional baseball wisdom says that a batting lineup composed of players with very diverse offensive skill sets will produce more runs than one composed of similar players (all other factors being equal). However, very little has been done to empirically validate or challenge this theory, partially due to a lack of tools worthy of answering the question. Within the emerging mathematical field of Topology, a computational tool in Topological Data Analysis (TDA) called Persistent Homology presents a robust method for measuring the diversity of a dataset. Using historical data from Major League Baseball teams and a python script designed to calculate the diversity of a dataset at the H_0 and H_1 levels of Persistent Homology, this centuries-old question can be addressed in a meaningful way.    Your title here   First Last  University Name   Abstract    The Limits of Complex Zeros of Steele Polynomials   Mica Goldstein  Willamette University  Simon Illouz  Willamette University, undergraduate  Peter Otto  Willamette University   The Steele Polynomial is a graph polynomial that arises in the study of minimal spanning trees and gives insights about the connectedness of the graph. In this talk, we present our research on the behavior of the limits of zeroes in the complex plane of the Steele polynomial for various infinite sequences of graphs. Our results use a recent generalization of the Beraha-Kahane-Weiss theorem that extends to the case of repeated roots of the characteristic polynomial of the recursion relationship satisfied by the Steele Polynomials.    Your title here   First Last  University Name   Abstract    Trees Defined by Their Distance Laplacian Spectrum   Skylar McLeod  Linfield University  Claire Heitman  Linfield University, Undergrad  Devin Doherty  Linfield University, Undergrad  Claire Schindler  Linfield University, Undergrad  Aiden Crossler  Linfield University, Undergrad  Kate Lorenzen  Linfield University   Graphs can be encoded into a matrix according to some rule. The eigenvalues of the matrix are used to understand the structural properties of graphs. If two graphs share a set of eigenvalues, they are called cospectral. A tree is a graph with no cycles, and for most matrix representations, almost all trees have a cospectral mate.  The distance Laplacian matrix is found by subtracting the distance matrix from the diagonal transmission matrix.  There is an open conjecture that trees, for their distance Laplacian matrices, do not have a cospectral mate and are therefore spectrally determined. We show that a family of trees of diameter 4 are determined by their spectrum.    Your title here   First Last  University Name   Abstract    Your title here   First Last  University Name   Abstract    Your title here   First Last  University Name   Abstract    Your title here   First Last  University Name   Abstract    Your title here   First Last  University Name   Abstract    Your title here   First Last  University Name   Abstract    Your title here   First Last  University Name   Abstract    Your title here   First Last  University Name   Abstract    Your title here   First Last  University Name   Abstract    Your title here   First Last  University Name   Abstract    Your title here   First Last  University Name   Abstract    Your title here   First Last  University Name   Abstract    Your title here   First Last  University Name   Abstract    Your title here   First Last  University Name   Abstract    "
+},
+{
+  "id": "contributed-talk-1a",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-1a",
+  "type": "Abstract",
+  "number": "2.1.1",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-1b",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-1b",
+  "type": "Abstract",
+  "number": "2.1.2",
+  "title": "Upper and Lower Bounds on Mosaic and Tile numbers of Knots.",
+  "body": " Upper and Lower Bounds on Mosaic and Tile numbers of Knots   Roland Allen  Gonzaga University  Maria Taggi Padro  Gonzaga University, Undergraduate   Mosaic Knot theory is a field where mathematical knots are placed on grid diagrams called mosaics. This was established by Dr. Lomonaco and Dr. Kauffman in Quantum Knots and Mosaics . In this work, we place improved lower and upper bounds on the size of a mosaic given only its crossing number or using crossing, braid index, and braid length. This is carried out by constructing an arbitrary diagram of a braid on a mosaic.  "
+},
+{
+  "id": "contributed-talk-2a",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-2a",
+  "type": "Abstract",
+  "number": "2.1.3",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-2b",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-2b",
+  "type": "Abstract",
+  "number": "2.1.4",
+  "title": "The Weighted Spanning Tree Enumerator of Multi-Partite Graphs.",
+  "body": " The Weighted Spanning Tree Enumerator of Multi-Partite Graphs   Zander Rasmussen  Willamette University  Peter Otto  Willamette University   The weighted spanning tree enumerator (WSTE) is the sum of weights of all spanning trees of a graph. A recent result expressed the WSTE of multi-partite graphs in terms of the WSTE of its partition graph, which greatly simplifies the computation of the WSTE of the original graph. The proof to this result is algebraic, using the Weighted Matrix Tree Theorem. In this talk, we will present the new result and the work we have done in deriving a graph theoretic justification for the result.  "
+},
+{
+  "id": "contributed-talk-3a",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-3a",
+  "type": "Abstract",
+  "number": "2.1.5",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-3b",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-3b",
+  "type": "Abstract",
+  "number": "2.1.6",
+  "title": "Geodesics and hyperbolicity for half-plane models of taxicab hyperbolic space.",
+  "body": " Geodesics and hyperbolicity for half-plane models of taxicab hyperbolic space   Alina Potapyev  Seattle University  Dylan Helliwell  Seattle University, Faculty  Luka Lawrence  Seattle University, Undergraduate Student  Kevin Lin  Seattle University, Undergraduate Student   We develop new geometric spaces by blending the half-plane model for the hyperbolic plane with taxicab geometry. Starting with a taxicab norm on each tangent space, the lengths of curves are analyzed, allowing us to determine geodesics, and from these, a distance function. Then we show that these spaces are hyperbolic in the sense of Gromov.  "
+},
+{
+  "id": "contributed-talk-4a",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-4a",
+  "type": "Abstract",
+  "number": "2.1.7",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-4b",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-4b",
+  "type": "Abstract",
+  "number": "2.1.8",
+  "title": "Applying Topological Data Analysis to Baseball Analytics: What Persistent Homology Says About the Need for Diverse Offensive Skill Sets.",
+  "body": " Applying Topological Data Analysis to Baseball Analytics: What Persistent Homology Says About the Need for Diverse Offensive Skill Sets   Levi Westeren  George Fox University  Thomas Shifley  George Fox University   Traditional baseball wisdom says that a batting lineup composed of players with very diverse offensive skill sets will produce more runs than one composed of similar players (all other factors being equal). However, very little has been done to empirically validate or challenge this theory, partially due to a lack of tools worthy of answering the question. Within the emerging mathematical field of Topology, a computational tool in Topological Data Analysis (TDA) called Persistent Homology presents a robust method for measuring the diversity of a dataset. Using historical data from Major League Baseball teams and a python script designed to calculate the diversity of a dataset at the H_0 and H_1 levels of Persistent Homology, this centuries-old question can be addressed in a meaningful way.  "
+},
+{
+  "id": "contributed-talk-5a",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-5a",
+  "type": "Abstract",
+  "number": "2.1.9",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-5b",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-5b",
+  "type": "Abstract",
+  "number": "2.1.10",
+  "title": "The Limits of Complex Zeros of Steele Polynomials.",
+  "body": " The Limits of Complex Zeros of Steele Polynomials   Mica Goldstein  Willamette University  Simon Illouz  Willamette University, undergraduate  Peter Otto  Willamette University   The Steele Polynomial is a graph polynomial that arises in the study of minimal spanning trees and gives insights about the connectedness of the graph. In this talk, we present our research on the behavior of the limits of zeroes in the complex plane of the Steele polynomial for various infinite sequences of graphs. Our results use a recent generalization of the Beraha-Kahane-Weiss theorem that extends to the case of repeated roots of the characteristic polynomial of the recursion relationship satisfied by the Steele Polynomials.  "
+},
+{
+  "id": "contributed-talk-6a",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-6a",
+  "type": "Abstract",
+  "number": "2.1.11",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-6b",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-6b",
+  "type": "Abstract",
+  "number": "2.1.12",
+  "title": "Trees Defined by Their Distance Laplacian Spectrum.",
+  "body": " Trees Defined by Their Distance Laplacian Spectrum   Skylar McLeod  Linfield University  Claire Heitman  Linfield University, Undergrad  Devin Doherty  Linfield University, Undergrad  Claire Schindler  Linfield University, Undergrad  Aiden Crossler  Linfield University, Undergrad  Kate Lorenzen  Linfield University   Graphs can be encoded into a matrix according to some rule. The eigenvalues of the matrix are used to understand the structural properties of graphs. If two graphs share a set of eigenvalues, they are called cospectral. A tree is a graph with no cycles, and for most matrix representations, almost all trees have a cospectral mate.  The distance Laplacian matrix is found by subtracting the distance matrix from the diagonal transmission matrix.  There is an open conjecture that trees, for their distance Laplacian matrices, do not have a cospectral mate and are therefore spectrally determined. We show that a family of trees of diameter 4 are determined by their spectrum.  "
+},
+{
+  "id": "contributed-talk-7a",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-7a",
+  "type": "Abstract",
+  "number": "2.1.13",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-7b",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-7b",
+  "type": "Abstract",
+  "number": "2.1.14",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-8a",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-8a",
+  "type": "Abstract",
+  "number": "2.1.15",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-8b",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-8b",
+  "type": "Abstract",
+  "number": "2.1.16",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-9a",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-9a",
+  "type": "Abstract",
+  "number": "2.1.17",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-9b",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-9b",
+  "type": "Abstract",
+  "number": "2.1.18",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-10a",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-10a",
+  "type": "Abstract",
+  "number": "2.1.19",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-10b",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-10b",
+  "type": "Abstract",
+  "number": "2.1.20",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-11a",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-11a",
+  "type": "Abstract",
+  "number": "2.1.21",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-11b",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-11b",
+  "type": "Abstract",
+  "number": "2.1.22",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-12a",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-12a",
+  "type": "Abstract",
+  "number": "2.1.23",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-12b",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-12b",
+  "type": "Abstract",
+  "number": "2.1.24",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-13a",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-13a",
+  "type": "Abstract",
+  "number": "2.1.25",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+},
+{
+  "id": "contributed-talk-13b",
+  "level": "2",
+  "url": "section-abstracts-contributed.html#contributed-talk-13b",
+  "type": "Abstract",
+  "number": "2.1.26",
+  "title": "Your title here.",
+  "body": " Your title here   First Last  University Name   Abstract  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
